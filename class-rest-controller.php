c88211e81c177ea0d52108f11018660325b997ea
Tomasz Dziuda <dziudek@gmail.com>
2017-06-01 21:16:51 +0200

    Initial commit

---
+++ class-rest-controller.php
@@ -0,0 +1,143 @@
+<?php
+
+class WP_Notes_Controller {
+    public function __construct() {
+        $this->namespace     = '/wp-notes/v1';
+        $this->resource_name = 'notes';
+    }
+
+    public function register_routes() {
+        register_rest_route( $this->namespace, '/' . $this->resource_name, array(
+            array(
+                'methods'   => 'GET',
+                'callback'  => array( $this, 'get_items' ),
+                'permission_callback' => array( $this, 'get_items_permissions_check' ),
+               'args' => array(
+                    'author' => array(
+                        'validate_callback' => function($param, $request, $key) {
+                            return is_numeric( $param );
+                        }
+                    )
+               )
+            ),
+            'schema' => array( $this, 'get_item_schema' ),
+        ));
+    }
+
+    public function get_items_permissions_check( $request ) {
+        if ( ! current_user_can( 'read_private_posts' ) ) {
+            return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot view the post resource.' ), array( 'status' => $this->authorization_status_code() ) );
+        }
+        return true;
+    }
+
+    public function get_items( $request ) {
+        $args = array(
+            'author' => $request->get_param( 'author' ),
+            'posts_per_page' => -1,
+            'post_type' => 'wp_notes',
+            'post_status' => 'private'
+        );
+        $posts = get_posts( $args );
+
+        $data = array();
+
+        if ( empty( $posts ) ) {
+            return rest_ensure_response( $data );
+        }
+
+        foreach ( $posts as $post ) {
+            $response = $this->prepare_item_for_response( $post, $request );
+            $data[] = $this->prepare_response_for_collection( $response );
+        }
+
+        // Return all of our comment response data.
+        return rest_ensure_response( $data );
+    }
+
+    public function get_item_permissions_check( $request ) {
+        if ( ! current_user_can( 'read' ) ) {
+            return new WP_Error( 'rest_forbidden', esc_html__( 'You cannot view the post resource.' ), array( 'status' => $this->authorization_status_code() ) );
+        }
+        return true;
+    }
+
+    public function prepare_item_for_response( $post, $request ) {
+        $post_data = array();
+
+        $schema = $this->get_item_schema( $request );
+
+        // We are also renaming the fields to more understandable names.
+        if ( isset( $schema['properties']['id'] ) ) {
+            $post_data['id'] = (int) $post->ID;
+        }
+
+        if ( isset( $schema['properties']['modificationDate'] ) ) {
+            $post_data['modificationDate'] = strtotime($post->post_modified_gmt . ' UTC') * 1000;
+        }
+
+        return rest_ensure_response( $post_data );
+    }
+
+    public function prepare_response_for_collection( $response ) {
+        if ( ! ( $response instanceof WP_REST_Response ) ) {
+            return $response;
+        }
+
+        $data = (array) $response->get_data();
+        $server = rest_get_server();
+
+        if ( method_exists( $server, 'get_compact_response_links' ) ) {
+            $links = call_user_func( array( $server, 'get_compact_response_links' ), $response );
+        } else {
+            $links = call_user_func( array( $server, 'get_response_links' ), $response );
+        }
+
+        if ( ! empty( $links ) ) {
+            $data['_links'] = $links;
+        }
+
+        return $data;
+    }
+
+
+    public function get_item_schema( $request ) {
+        $schema = array(
+            '$schema'              => 'http://json-schema.org/draft-04/schema#',
+            'title'                => 'note',
+            'type'                 => 'object',
+            'properties'           => array(
+                'id' => array(
+                    'description'  => esc_html__( 'Unique identifier for the object.', 'dziudek-wp-notes' ),
+                    'type'         => 'integer',
+                    'context'      => array( 'view', 'edit', 'embed' ),
+                    'readonly'     => true
+                ),
+                'modificationDate' => array(
+                    'description'  => esc_html__( 'The GMT time of the last post modification.', 'dziudek-wp-notes' ),
+                    'type'         => 'integer'
+                )
+            ),
+        );
+
+        return $schema;
+    }
+
+    public function authorization_status_code() {
+
+        $status = 401;
+
+        if ( is_user_logged_in() ) {
+            $status = 403;
+        }
+
+        return $status;
+    }
+}
+
+function dziudek_wp_notes_register_my_rest_routes() {
+    $controller = new WP_Notes_Controller();
+    $controller->register_routes();
+}
+
+add_action( 'rest_api_init', 'dziudek_wp_notes_register_my_rest_routes' );
---
+++ dziudek-wp-notes.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Dziudek WP Notes
+ * Plugin URI:        http://dziudek.pl
+ * Description:       Plugin used to create CPT and endpoints for the WP Notes Electron App
+ * Version:           1.0.0
+ * Author:            Tomasz Dziuda
+ * Author URI:        http://dziudek.pl
+ * License:           GPL-3.0
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       dziudek-wp-notes
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+    die;
+}
+
+require plugin_dir_path(__FILE__) . 'class-rest-controller.php';
+
+// Register Custom Post Type
+function dziudek_wp_notes_cpt() {
+    $labels = array(
+        'name'                  => _x( 'Notes', 'Post Type General Name', 'dziudek_wp_notes_cpt' ),
+        'singular_name'         => _x( 'Note', 'Post Type Singular Name', 'dziudek_wp_notes_cpt' ),
+        'menu_name'             => __( 'Notes', 'dziudek_wp_notes_cpt' ),
+        'name_admin_bar'        => __( 'Notes', 'dziudek_wp_notes_cpt' ),
+        'archives'              => __( 'Notes archive', 'dziudek_wp_notes_cpt' ),
+        'attributes'            => __( 'Note attributes', 'dziudek_wp_notes_cpt' ),
+        'parent_item_colon'     => __( 'Parent note:', 'dziudek_wp_notes_cpt' ),
+        'all_items'             => __( 'All notes', 'dziudek_wp_notes_cpt' ),
+        'add_new_item'          => __( 'Add New Note', 'dziudek_wp_notes_cpt' ),
+        'add_new'               => __( 'Add New', 'dziudek_wp_notes_cpt' ),
+        'new_item'              => __( 'New Note', 'dziudek_wp_notes_cpt' ),
+        'edit_item'             => __( 'Edit Note', 'dziudek_wp_notes_cpt' ),
+        'update_item'           => __( 'Update Note', 'dziudek_wp_notes_cpt' ),
+        'view_item'             => __( 'View Note', 'dziudek_wp_notes_cpt' ),
+        'view_items'            => __( 'View Notes', 'dziudek_wp_notes_cpt' ),
+        'search_items'          => __( 'Search Notes', 'dziudek_wp_notes_cpt' ),
+        'not_found'             => __( 'Not found', 'dziudek_wp_notes_cpt' ),
+        'not_found_in_trash'    => __( 'Not found in Trash', 'dziudek_wp_notes_cpt' ),
+        'featured_image'        => __( 'Featured Image', 'dziudek_wp_notes_cpt' ),
+        'set_featured_image'    => __( 'Set featured image', 'dziudek_wp_notes_cpt' ),
+        'remove_featured_image' => __( 'Remove featured image', 'dziudek_wp_notes_cpt' ),
+        'use_featured_image'    => __( 'Use as featured image', 'dziudek_wp_notes_cpt' ),
+        'insert_into_item'      => __( 'Insert into note', 'dziudek_wp_notes_cpt' ),
+        'uploaded_to_this_item' => __( 'Uploaded to this note', 'dziudek_wp_notes_cpt' ),
+        'items_list'            => __( 'Notes list', 'dziudek_wp_notes_cpt' ),
+        'items_list_navigation' => __( 'Notes list navigation', 'dziudek_wp_notes_cpt' ),
+        'filter_items_list'     => __( 'Filter notes list', 'dziudek_wp_notes_cpt' ),
+    );
+
+    $args = array(
+        'label'                 => __( 'Note', 'dziudek_wp_notes_cpt' ),
+        'description'           => __( 'For WP Notes Electron App', 'dziudek_wp_notes_cpt' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'editor', 'author', 'revisions', ),
+        'taxonomies'            => array( 'category', 'post_tag' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 5,
+        'menu_icon'             => 'dashicons-media-default',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => false,
+        'exclude_from_search'   => true,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'page',
+        'show_in_rest'          => true,
+        'rest_base'             => 'wp-notes',
+    );
+
+    register_post_type( 'wp_notes', $args );
+}
+
+add_action( 'init', 'dziudek_wp_notes_cpt', 0 );
+
+function dziudek_wp_notes_use_raw_content( $data, $post, $request ) {
+    unset($data->data['content']['rendered']);
+    unset($data->data['title']['rendered']);
+    $data->data['content']['plaintext'] = $post->post_content;
+    $data->data['title']['plaintext'] = $post->post_title;
+    $data->data['modified_gmt'] = strtotime($post->post_modified_gmt . ' UTC') * 1000;
+    return $data;
+}
+
+add_filter( 'rest_prepare_wp_notes', 'dziudek_wp_notes_use_raw_content', 10, 3 );
